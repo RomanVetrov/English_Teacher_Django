87d6633aea895f432c985e0023949c96b88760bc
RomanVetrov <romanvikernes@gmail.com>
2025-11-22 22:03:56 +0300

    Стартовая инициализация

---
+++ core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(
+        request,
+        'core.index.html'
+    )
+
+
+def about(request):
+    return render(
+        request,
+        'core.about.html'
+    )
+
+
+def life(request):
+    return render(
+        request,
+        'core.life.html'
+    )
+
+
+def contacts(request):
+    return render(
+        request,
+        'core.contacts.html'
+    )
