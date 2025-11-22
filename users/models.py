81653faf62abd69ff9f6e1456e2def2a5b5c4c60
RomanVetrov <romanvikernes@gmail.com>
2025-11-23 02:01:18 +0300

    feat: кастомная модель User с email-login

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Кастомный менеджер для User без username"""
+    
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email обязателен")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None  # убираем username
+    email = models.EmailField("Email", unique=True)
+    
+    objects = UserManager()  # используем кастомный менеджер
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # убираем username из обязательных
+
+    def __str__(self):
+        return self.email
+
