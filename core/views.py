eeb6ed2661d3fff22f0b03e1950e853d57d62e25
RomanVetrov <romanvikernes@gmail.com>
2025-11-22 23:02:18 +0300

    Добавлены стартовые html/css в base, index

--- core/views.py
+++ core/views.py
@@ -3,26 +3,26 @@ from django.shortcuts import render
 def index(request):
     return render(
         request,
-        'core.index.html'
+        'core/index.html'
     )
 
 
 def about(request):
     return render(
         request,
-        'core.about.html'
+        'core/about.html'
     )
 
 
 def life(request):
     return render(
         request,
-        'core.life.html'
+        'core/life.html'
     )
 
 
 def contacts(request):
     return render(
         request,
-        'core.contacts.html'
+        'core/contacts.html'
     )
