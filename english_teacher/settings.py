b475877e924df7bca4623031858ca87b5641bd97
RomanVetrov <romanvikernes@gmail.com>
2025-11-23 21:36:12 +0300

    fix security

--- english_teacher/settings.py
+++ english_teacher/settings.py
@@ -19,11 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(*acqsxh308xd$xio&s2lkf6rpenj+1z1x+u=dowhf$=3-b&u&'
+import os
+from dotenv import load_dotenv
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+load_dotenv()
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
